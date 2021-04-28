93a6687333643c30131a35e8698351f7abbedc39
imadwr <imadouairem96@gmail.com>
2021-04-28 17:10:58 +0000

    Add scoreboard and increase ball speed

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
+
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -12,6 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -23,7 +26,7 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.05)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -32,13 +35,14 @@ while game_is_on:
 
     if (ball.distance(r_paddle) < 50 and ball.xcor() > 320) or (ball.distance(l_paddle) < 50 and ball.xcor() < -320):
         ball.bounce_paddle()
-
 # right side
     if ball.xcor() > 390:
         ball.reset_position()
+        scoreboard.r_point()
 # left side
     if ball.xcor() < -390:
         ball.reset_position()
+        scoreboard.l_point()
 
 
 screen.exitonclick()
